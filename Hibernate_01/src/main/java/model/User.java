173bba546f5b74bcf0916c91b7bc2f4bd1a320dd
Tuan Lai <tuanlaimanh20041@gmail.com>
2026-01-09 00:00:28 +0700

    feat: create a new entity (User)

---
+++ Hibernate_01/src/main/java/model/User.java
@@ -0,0 +1,64 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "User")
+public class User {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private double salary;
+
+    public User() {};
+
+    public User(Integer id, String name, double salary, String email) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Id  // primary key
+    @GeneratedValue  // auto-increment
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
